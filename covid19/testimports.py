101e734685a48b906f1fb90a3105dcf2943e1c91
Adiv Paradise <paradise@astro.utoronto.ca>
2021-12-23 18:04:36 -0500

    Adding resource limits to import

--- covid19/testimports.py
+++ covid19/testimports.py
@@ -1,12 +1,15 @@
-import numpy as np
-from scipy.special import factorial,loggamma
-import matplotlib.pyplot as plt
-import csv, os
-from datetime import date, timedelta
-import warnings,traceback
 
 if __name__=="__main__":
+   os.system(' echo "beginning imports">/home/adivp416/public_html/covid19/pythonlog.txt')
+   import os 
+   os.environ['OPENBLAS_NUM_THREADS'] = '2'
+   import numpy as np
+   from scipy.special import factorial,loggamma
+   import matplotlib.pyplot as plt
+   import csv
+   from datetime import date, timedelta
+   import warnings,traceback
 
-   os.system("echo Initialized>pythonlog.txt")
+   os.system("echo Initialized>>/home/adivp416/public_html/covid19/pythonlog.txt")
    x = np.arange(10)
-   os.system("echo numpy>>pythonlog.txt")
+   os.system("echo numpy>>/home/adivp416/public_html/covid19/pythonlog.txt")
