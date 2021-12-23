73737b634fa757f7c7dcd529778f2b5aecf16397
Adiv Paradise <paradise@astro.utoronto.ca>
2021-12-23 18:59:58 -0500

    Updating report builder and fixing resource internal links

--- covid19/makereports.sh
+++ covid19/makereports.sh
@@ -12,16 +12,17 @@ wget -O /home/adivp416/public_html/covid19/github/time_series_covid19_deaths_glo
 wget -O /home/adivp416/public_html/covid19/github/time_series_covid19_recovered_global.csv https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv
 wget -O /home/adivp416/public_html/covid19/github/time_series_covid19_deaths_US.csv https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv
 
-sed -i 's/"Korea, South"/South Korea/g' time_series_covid19_confirmed_global.csv
-sed -i 's/"Korea, South"/South Korea/g' time_series_covid19_deaths_global.csv
-sed -i 's/Taiwan\*/Taiwan/g' time_series_covid19_confirmed_global.csv
-sed -i 's/Taiwan\*/Taiwan/g' time_series_covid19_deaths_global.csv
-rm *.csv.?
-cd ../
-wget -O toronto_cases.csv https://ckan0.cf.opendata.inter.prod-toronto.ca/datastore/dump/e5bf35bc-e681-43da-b2ce-0242d00922ad
-wget -O ontario_cases.csv https://data.ontario.ca/dataset/1115d5fe-dd84-4c69-b5ed-05bf0c0a0ff9/resource/d1bfe1ad-6575-4352-8302-09ca81f7ddfc/download/cases_by_status_and_phu.csv
+sed -i 's/"Korea, South"/South Korea/g' /home/adivp416/public_html/covid19/github/time_series_covid19_confirmed_global.csv
+sed -i 's/"Korea, South"/South Korea/g' /home/adivp416/public_html/covid19/github/time_series_covid19_deaths_global.csv
+sed -i 's/Taiwan\*/Taiwan/g' /home/adivp416/public_html/covid19/github/time_series_covid19_confirmed_global.csv
+sed -i 's/Taiwan\*/Taiwan/g' /home/adivp416/public_html/covid19/github/time_series_covid19_deaths_global.csv
+cd /home/adivp416/public_html/covid19/
+wget -O /home/adivp416/public_html/covid19/toronto_cases.csv https://ckan0.cf.opendata.inter.prod-toronto.ca/datastore/dump/e5bf35bc-e681-43da-b2ce-0242d00922ad
+wget -O /home/adivp416/public_html/covid19/ontario_cases.csv https://data.ontario.ca/dataset/1115d5fe-dd84-4c69-b5ed-05bf0c0a0ff9/resource/d1bfe1ad-6575-4352-8302-09ca81f7ddfc/download/cases_by_status_and_phu.csv
+
 cd $cdir
 
 cd /home/adivp416/public_html/covid19
-python covid19report.py
+python /home/adivp416/public_html/covid19/covid19report.py
+cat /home/adivp416/public_html/covid19/reportlog.txt
 cd $cdir
