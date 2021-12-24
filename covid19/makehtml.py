4d710fbef0f597f22f52458d17d20490dab578a8
Adiv Paradise <paradise@astro.utoronto.ca>
2021-12-24 01:52:09 -0500

    Fixed duplicate plots

--- covid19/makehtml.py
+++ covid19/makehtml.py
@@ -63,14 +63,14 @@ def makeneighborhood(neighborhood,pathdir):
                       "Raw and 2-week average of %s and Toronto's effective reproductive numbers. This is the average number of people a sick person will infect. If this is increasing, then transmission is increasing, even if cases are still declining. If this is above 1, then cases are increasing. If it is decreasing, then transmission is declining, even if cases are still rising. Note that due to the existence of super-spreaders, this metric is not the same as the number of people the average sick person will infect (i.e. a person selected at random from the cohort of infected people will typically infect fewer people than would be implied by R<sub>t</sub>, but a small fraction will infect many more."%neighborhood,"%s_Rt.png"%pathdir,
                       "%s_Rt.pdf"%pathdir)+"\n"+
             "<br> \n"+
-            img2plate%("%s_avgcases.png"%pathdir,
-                      "%s average cases per day, linear scale"%neighborhood,
-                      "%s_avgcases_log.png"%pathdir,
-                      "%s_average cases per day, logarithmic scale"%neighborhood,
-                      "7-day average of new COVID-19 cases per day in %s, Toronto."%neighborhood+
+            img2plate%("%s_relcases.png"%pathdir,
+                      "%s average cases per day per 100k, linear scale"%neighborhood,
+                      "%s_relcases_log.png"%pathdir,
+                      "%s_average cases per day per 100k, logarithmic scale"%neighborhood,
+                      "7-day average of new COVID-19 cases per day per 100k in %s, Toronto."%neighborhood+
                       "These data have had a rolling average applied. The first plot gives the data on a linear vertical scale, while the second gives the data on a logarithmic vertical scale, which better shows different exponentials.",
-                      "%s_avgcases.png"%pathdir,"%s_avgcases.pdf"%pathdir,
-                      "%s_avgcases_log.png"%pathdir,"%s_avgcases_log.pdf"%pathdir)+"\n"+
+                      "%s_relcases.png"%pathdir,"%s_relcases.pdf"%pathdir,
+                      "%s_relcases_log.png"%pathdir,"%s_relcases_log.pdf"%pathdir)+"\n"+
             "<br >\n"+
             "<p>Last updated %s</p>"%(time.asctime(time.localtime())))
             
