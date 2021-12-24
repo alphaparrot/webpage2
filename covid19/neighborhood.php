5a3434f8ff4956501c6a49b4df2256d525441d46
Adiv Paradise <paradise@astro.utoronto.ca>
2021-12-24 02:14:51 -0500

    argh

--- covid19/neighborhood.php
+++ covid19/neighborhood.php
@@ -5,11 +5,12 @@ $neighborhood = $_POST['name'];
 $filename = str_replace(" ","_",$neighborhood);
 $filename = str_replace("-","_",$filename);
 $filename = str_replace("+","_",$filename);
+echo $filename
 if($neighborhood !=''&& $neighborhood !='placeholder')
 {
 echo $filename."html" //must delete for Header to work
 //  To redirect form on a particular page
-header("Location:".$filename.".html");
+// header("Location:".$filename.".html");
 }
 else{
 ?><span><?php echo "Please select a neighborhood!";?></span> <?php
