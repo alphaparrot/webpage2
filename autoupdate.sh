e3432706fbf4abbf3d29735b842093995605d97c
Adiv Paradise <paradise@astro.utoronto.ca>
2021-12-25 11:13:07 -0500

    Cleanup

--- autoupdate.sh
+++ autoupdate.sh
@@ -2,5 +2,6 @@
 
 cd /home/adivp416/public_html/
 find /home/adivp416/public_html/covid19/ -type d -empty -print0 | xargs -0 -I {} /bin/rmdir "{}"
+rm -rf /home/adivp416/public_html/covid19/ontario_*
 git stash
 git pull
