3d0d6c3ef327bcfd28e785dedcc9fd90e125b96d
Adiv Paradise <paradise@astro.utoronto.ca>
2021-12-30 22:26:43 -0500

    Adding tmpdir

--- bootstrapenv.sh
+++ bootstrapenv.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
+mkdir /home/adivp416/tmpconda
+TMPDIR=/home/adivp416/tmpconda
 /home/adivp416/public_html/Miniconda3-py39_4.10.3-Linux-x86_64.sh -b -p /home/adivp416/miniconda
 #python -m pip install --user numpy scipy matplotlib
