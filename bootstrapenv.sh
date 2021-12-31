ca4bb3b8a050d47ca16e4800a27f2fdae891f827
Adiv Paradise <paradise@astro.utoronto.ca>
2021-12-30 22:31:25 -0500

    fix #2

--- bootstrapenv.sh
+++ bootstrapenv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir /home/adivp416/tmpconda
-TMPDIR=/home/adivp416/tmpconda
-/home/adivp416/public_html/Miniconda3-py39_4.10.3-Linux-x86_64.sh -u -b -p /home/adivp416/miniconda
+TMP=/home/adivp416/tmpconda
+TMPDIR=/home/adivp416/tmpconda bash /home/adivp416/public_html/Miniconda3-py39_4.10.3-Linux-x86_64.sh -u -b -p /home/adivp416/miniconda
 #python -m pip install --user numpy scipy matplotlib
