b8f9008f4f63f0fa3bd00cc0f8a01b15349983fa
Adiv Paradise <paradise@astro.utoronto.ca>
2021-12-30 22:17:55 -0500

    Miniconda bootstrapper

--- bootstrapenv.sh
+++ bootstrapenv.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python -m pip install --user numpy scipy matplotlib
+/home/adivp416/public_html/Miniconda3-py39_4.10.3-Linux-x86_64.sh -b -p /home/adivp416/miniconda
+#python -m pip install --user numpy scipy matplotlib
