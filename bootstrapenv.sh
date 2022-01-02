f19e09284272b2f7ab486060574500b73f065dd6
Adiv Paradise <paradise@astro.utoronto.ca>
2022-01-02 12:33:41 -0500

    Trying pip install configparser

--- bootstrapenv.sh
+++ bootstrapenv.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-mkdir /home/adivp416/tmpconda
-TMP=/home/adivp416/tmpconda
-TMPDIR=/home/adivp416/tmpconda bash /home/adivp416/public_html/Miniconda3-py39_4.10.3-Linux-x86_64.sh -u -b -p /home/adivp416/miniconda
+#mkdir /home/adivp416/tmpconda
+#TMP=/home/adivp416/tmpconda
+#TMPDIR=/home/adivp416/tmpconda bash /home/adivp416/public_html/Miniconda3-py39_4.10.3-Linux-x86_64.sh -u -b -p /home/adivp416/miniconda
 #python -m pip install --user numpy scipy matplotlib
+cd /home/adivp416/public_html/
+python -m pip install --user configparser
+python -m pip install --user -U netCDF4
