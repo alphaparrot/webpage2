92f8bc3235d7ef4dcfaa8926d1ab05b4298c0f8e
Adiv Paradise <paradise@astro.utoronto.ca>
2022-01-13 18:15:38 -0500

    Temporarily removing ulimit

--- covid19/makereports.sh
+++ covid19/makereports.sh
@@ -4,8 +4,6 @@
 
 cd /home/adivp416/public_html/covid19
 
-ulimit -v 500000
-
 ionice -c 3 python /home/adivp416/public_html/covid19/covid19report.py report
 cat /home/adivp416/public_html/covid19/reportlog.txt
 cd $cdir
